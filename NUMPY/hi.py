3a0d373fd16c86b42ac8e7d2a52e209f4be19292
DHARSH0205 <dharshinisampathkumarklt@gmail.com>
2024-10-20 22:28:48 +0530

    it will show

---
+++ NUMPY/hi.py
@@ -0,0 +1,6 @@
+print("hello new to hub")
+ins1 = "git clone"
+ins2 = "git add fileName.extension"
+status = "git status"
+ins3 = "git push origin [to add to main branch]"
+ins = "git commit -m 'message to tell"
